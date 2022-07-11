55956c19acb159a55c8f79f1d131b88ea52c76b8
Liz <elizabethlfransen@gmail.com>
2022-07-11 14:29:45 -0500

    #15 Format register helper constructor args

--- src/main/kotlin/io/github/elizabethlfransen/lizlib/register/RegisterHelper.kt
+++ src/main/kotlin/io/github/elizabethlfransen/lizlib/register/RegisterHelper.kt
@@ -7,7 +7,11 @@ import kotlin.properties.ReadOnlyProperty
  * Used to quickly get registers on demand.
  * A RegisterHelper is tied to a specific mod, specified by [modId] and a supplied map for getting a register by type.
  */
-class RegisterHelper(private val modId: String, private val registerFactory: IDeferredRegisterFactory, private val registerProviders: Map<Class<*>, RegisterProvider<*>>) {
+class RegisterHelper(
+    private val modId: String,
+    private val registerFactory: IDeferredRegisterFactory,
+    private val registerProviders: Map<Class<*>, RegisterProvider<*>>
+) {
     private val registers = mutableMapOf<Class<*>, IDeferredRegister<*>>()
     private val eventBuses = mutableSetOf<IEventBus>()
 
@@ -30,10 +34,10 @@ class RegisterHelper(private val modId: String, private val registerFactory: IDe
     @Suppress("UNCHECKED_CAST")
     fun <T> resolveRegister(registerType: Class<T>): IDeferredRegister<T> {
         // if register already exists return it
-        if(registers.containsKey(registerType))
+        if (registers.containsKey(registerType))
             return registers[registerType] as IDeferredRegister<T>
         // if no register provider exists for type throw an error
-        if(!registerProviders.containsKey(registerType))
+        if (!registerProviders.containsKey(registerType))
             throw IllegalArgumentException("No register provider for type \"${registerType.simpleName}\"")
         // register must be created
         val register = registerProviders[registerType]!!(modId, registerFactory)
@@ -43,17 +47,17 @@ class RegisterHelper(private val modId: String, private val registerFactory: IDe
         registers[registerType] = register
         return register as IDeferredRegister<T>
     }
+
     /**
      * Resolves a register of type [T]. If the register has already used it's not recreated. If there is no
      * provider of type [T], [IllegalArgumentException] is thrown.
      */
-    inline fun <reified T> resolveRegister()
-        = resolveRegister(T::class.java)
+    inline fun <reified T> resolveRegister() = resolveRegister(T::class.java)
 
-    fun <T> registerObject(objectType: Class<T>, name: String, supplier: () -> T): ReadOnlyProperty<Any?, T>
-        = resolveRegister(objectType)
+    fun <T> registerObject(objectType: Class<T>, name: String, supplier: () -> T): ReadOnlyProperty<Any?, T> =
+        resolveRegister(objectType)
             .registerObject(name, supplier)
 
-    inline fun <reified T> registerObject(name: String, noinline supplier: () -> T): ReadOnlyProperty<Any?, T>
-        = registerObject(T::class.java, name, supplier)
+    inline fun <reified T> registerObject(name: String, noinline supplier: () -> T): ReadOnlyProperty<Any?, T> =
+        registerObject(T::class.java, name, supplier)
 }
